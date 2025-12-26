7d18e1ae727b030b33c55a5b878c8e29358ba6f3
harshith <harshith7422@gmail.com>
2025-12-26 14:18:36 +0530

    DOM basics

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela"
+
+document.querySelector("h1").classList.toggle("huge")
+
+document.querySelector("h1").innerHTML = "harshith"
