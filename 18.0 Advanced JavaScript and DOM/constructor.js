a4e02954938549213e27295d88417b3ec364c26b
harshith <harshith7422@gmail.com>
2025-12-29 16:16:50 +0530

    js advanced and DOM

---
+++ 18.0 Advanced JavaScript and DOM/constructor.js
@@ -0,0 +1,10 @@
+// constructor function
+
+function BellBoy(name, age, yearsOfExperience, languagesKnown) {
+    this.name = name;
+    this.age = age;
+    this.yearsOfExperience = yearsOfExperience;
+    this.languagesKnown = languagesKnown;
+}
+let bellboy1 = new BellBoy("harshith", "22", "5", ["hindi", "telugu"])
+console.log(bellboy1)
