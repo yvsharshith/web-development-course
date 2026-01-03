c3793c402c5613a9f4e2d5d2f701409b8ced18f7
harshith <harshith7422@gmail.com>
2026-01-03 14:48:32 +0530

    npm modules usage(example: superheros pack)

---
+++ 23.2 NPM/index.js
@@ -0,0 +1,13 @@
+/*
+let generateName = require("sillyname");
+import generateName from "sillyname";
+
+let sillyname = generateName();
+
+console.log(`My name is ${sillyname}.`)
+*/
+
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+
+console.log(`Iam ${name}!`)
