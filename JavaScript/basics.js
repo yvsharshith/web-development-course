bd0471cdeb414a7157a69e26e454a338eabd7b4a
harshith <harshith7422@gmail.com>
2026-01-14 17:03:04 +0530

    js practice

---
+++ JavaScript/basics.js
@@ -0,0 +1,26 @@
+/*
+
+let a = 5 // let is for declaring variables
+let b = 8
+let sum = a + b
+console.log(sum) // to print the ans
+
+let carName = "Volvo"
+const price = 1250000
+console.log(carName + "is of " + price + "cost")
+
+//arthematic operations
+let a = 8
+let b = 5
+let add, sub, div, mul
+add = a + b
+console.log(add)
+sub = a - b
+console.log(sub)
+mul = a * b
+console.log(mul)
+div = a / b
+console.log(div)
+
+*/
+
